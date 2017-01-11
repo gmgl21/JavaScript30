bce43a37c4d86b28144b905fe3f19fb446bb601a
Gloria Lee <gmgl21@hotmail.com>
2017-01-10 19:48:40 -0800

    Add files via upload

---
+++ Day 11 Challenge Custom HTML5 Video Player/scripts.js
@@ -0,0 +1,82 @@
+/*Get Our Elements*/
+
+const player=document.querySelector('.player');
+const video=player.querySelector('.viewer');
+const progress=player.querySelector('.progress');
+const progressBar=player.querySelector('.progress__filled');
+
+const toggle=player.querySelector('.toggle');
+const skipButtons=player.querySelectorAll('[data-skip]');
+const ranges=player.querySelectorAll('.player__slider');
+
+const fullscreen=player.querySelector('.fullscreen');
+
+/*Build out our function*/
+function togglePlay(){
+	const method= video.paused ? 'play' : 'pause';
+	video[method]();
+}
+/*	if(video.paused){
+		video.play();
+	}else{
+		video.pause();
+	}
+}*/
+function updateButton(){
+	const icon= this.paused ? '►' : '❚ ❚';
+	toggle.textContent=icon;
+	console.log(icon);
+}
+
+function skip(){
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(){
+	video[this.name]=this.value;
+	//console.log(this.name);
+	//console.log(this.value);
+}
+
+function handleProgress(){
+	const percent=(video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis=`${percent}%`;
+}
+
+function scrub(e){
+	const scrubTime=(e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime=scrubTime;
+	console.log(e);
+}
+
+function toggleFullScreen() {
+  if (video.requestFullscreen) {
+      video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen(); 
+  }else if (video.mozRequestFullscreen) {
+      video.mozRequestFullscreen(); 
+  }
+}
+
+
+
+/*Hook up the event listeners*/
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click',skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown=false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e)=> mousedown && scrub(e));
+progress.addEventListener('mousedown', ()=> mousedown=true);
+progress.addEventListener('mouseup', ()=> mousedown=false);
+
+fullscreen.addEventListener('click', toggleFullScreen, false);
